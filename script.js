e0e5dd9562dc93c02605851ec256a48e43335cff
carlospeyrot03 <carlospeyrot0803@gmail.com>
2024-08-08 12:17:22 -0300

    Hemos hecho una web de consulta de clima para uso de JS

---
+++ script.js
@@ -0,0 +1,51 @@
+const urlBase = `https://api.openweathermap.org/data/2.5/weather`;
+const API_KEY = "7e331d3dcf21a0720e06fe4c1fec5969";
+const diffKelvin = 273.15;
+
+document.getElementById("searchButton").addEventListener("click", () => {
+    const city = document.getElementById("cityInput").value;
+    if(city) {
+        fetchWeather(city);
+    } else {
+        alert("Ingrese una ciudad existente");
+    }
+});
+
+function fetchWeather(city) {
+    fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
+    .then(data => data.json())
+    .then(data => showWeatherData(data))
+};
+
+function showWeatherData(data) {
+    const divResponseData = document.getElementById("responseData");
+    divResponseData.innerHTML = "";
+
+    const cityName = data.name;
+    const countryName = data.sys.country;
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+
+    const cityInfo = document.createElement("h2");
+    cityInfo.textContent = `${cityName}, ${countryName}`;
+
+    const tempInfo = document.createElement("p");
+    tempInfo.textContent = `La temperatura es de: ${Math.floor(temp - diffKelvin)} grados Celsius`;
+
+    const humidityInfo = document.createElement("p");
+    humidityInfo.textContent = `La humedad es del: ${humidity}`;
+
+    const iconInfo = document.createElement("img");
+    iconInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+    const descriptionInfo = document.createElement("p");
+    descriptionInfo.textContent = `La descripcion meteorologica es: ${description}`;
+
+    divResponseData.appendChild(cityInfo);
+    divResponseData.appendChild(tempInfo);
+    divResponseData.appendChild(humidityInfo);
+    divResponseData.appendChild(iconInfo);
+    divResponseData.appendChild(descriptionInfo);
+}
